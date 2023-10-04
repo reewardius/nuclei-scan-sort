4afa42b61195a98c957c48af9efab8972616f1f4
DIMOOON <68978608+reewardius@users.noreply.github.com>
2023-10-04 22:10:40 +0300

    Update nuclei_sort.py

--- nuclei_sort.py
+++ nuclei_sort.py
@@ -2,102 +2,77 @@ import sys
 from urllib.parse import urlparse
 from colorama import Fore, Style, init
 from argparse import ArgumentParser
+from collections import defaultdict
 
-
-def sort_by_url(scan_results):
-    try:
-        return sorted(scan_results, key=lambda x: x.split(" ")[3])
-    except IndexError:
-        # Check if scan file is valid
-        print(Fore.RED + 'Not valid Nuclei Scan format' + Style.RESET_ALL)
-        sys.exit(1)
-
-
-def sort_by_severity(scan_results):
-    severity_order = {'[critical]': 1, '[high]': 2, '[medium]': 3, '[low]': 4, '[info]': 5}
-    return sorted(scan_results, key=lambda x: severity_order[x.split(" ")[2]])
-
-
-def sort_by_all(scan_results):
-    unique_domains = {}
-    sorted_results = []
-    sorted_by_url = sort_by_url(scan_results)
-
-    for i in sorted_by_url:
-        # Find all urls
-        parts_of_url = urlparse(i.split(" ")[3])
-        domains = []
-        # Add to "domains" list parts of url without http:// or https://
-        if parts_of_url.netloc != '':
-            domains.append(parts_of_url.netloc)
-        else:
-            domains.append(parts_of_url.path)
-        # Create new list to split all unique domains
-        for domain in domains:
-            if domain not in unique_domains:
-                unique_domains[domain] = [i]
-            else:
-                unique_domains[domain].append(i)
-
-    # Sort by severity for all unique domains
-    for i in unique_domains.values():
-        by_severity = sort_by_severity(i)
-        # Add delimiter between different domains
-        by_severity[-1] += '\n'
-        for j in by_severity:
-            sorted_results.append(j)
-
-    return sorted_results
-
+def parse_severity(severity):
+    severity_order = {'[critical]': 1, '[high]': 2, '[medium]': 3, '[low]': 4, '[info]': 5, '[unknown]': 6, '[]': 7}
+    return severity_order.get(severity, 7)
 
 def main(input_file):
+    try:
+        # Read file with nuclei scan and split it into lines
+        with open(input_file, 'r') as file:
+            scan_results = [result.strip() for result in file.readlines()]
+    except FileNotFoundError:
+        print(Fore.RED + '[!]  Not existing file or path' + Style.RESET_ALL)
+        sys.exit(1)
+
+    unique_domains = defaultdict(list)
     garbage_info = []
-    # Read file with nuclei scan and append to the list
-    with open(input_file, 'r') as file:
-        scan_results = [result.strip() for result in file.readlines()]
 
-    for i in scan_results:
-        # Check if there is garbage information in [INF] brackets
-        if i.split(" ")[0] == '[INF]':
-            # Append it to "garbage_info" list and remove from original one
-            garbage_info.append('Garbage: ' + i)
-            scan_results.remove(i)
-            print(garbage_info)
+    for result in scan_results[:]:
+        parts = result.split(" ")
+        if len(parts) < 4:
+            # Check if scan file is valid
+            print(Fore.RED + 'Not valid Nuclei Scan format' + Style.RESET_ALL)
+            sys.exit(1)
+        
+        severity, _, url = parts[2], parts[3], parts[3]
+        
+        if severity == '[INF]':
+            # Handle garbage information
+            garbage_info.append('Garbage: ' + result)
+            scan_results.remove(result)
         else:
-            continue
+            # Extract domain from URL
+            parsed_url = urlparse(url)
+            domain = parsed_url.netloc if parsed_url.netloc else parsed_url.path
+            unique_domains[domain].append((severity, url))
 
-    # Identify the color of each severity
     color_map = {
         '[critical]': Fore.MAGENTA,
         '[high]': Fore.RED,
         '[medium]': Fore.YELLOW,
         '[low]': Fore.GREEN,
-        '[info]': Fore.CYAN
+        '[info]': Fore.CYAN,
+        '[unknown]': Fore.WHITE,
+        '[]': Fore.WHITE
     }
 
-    # Sort by severity and URL and split the list
-    results = [i.split(" ") for i in sort_by_all(scan_results)]
+    sorted_results = []
 
-    # Move [severity] to the first position, color with color_map and remove from original position
-    for i in results:
-        print(color_map.get(i[2]) + i[2] + Style.RESET_ALL + " " + " ".join(i).replace(i[2], ''))
+    for domain, results in unique_domains.items():
+        results.sort(key=lambda x: parse_severity(x[0]))
+        for severity, url in results:
+            sorted_results.append((domain, severity, url))
 
+    sorted_results.sort(key=lambda x: (parse_severity(x[1]), x[2]))
+
+    for domain, severity, url in sorted_results:
+        color = color_map.get(severity, Fore.WHITE)
+        print(f"{color}{severity}{Style.RESET_ALL} {url}")
+
+    for garbage in garbage_info:
+        print(garbage)
 
 if __name__ == '__main__':
-    # Colorama function to color text in CMD
     init()
-    # Available arguments(in CMD type -h for help)
     parser = ArgumentParser()
     parser.add_argument('-i', '--input', help='Input file with Nuclei scan: /home/kali/scan.txt', default='', required=True)
     args = parser.parse_args()
 
-    # Check if file or path is valid and sort it
     if args.input != '':
-        try:
-            main(args.input)
-        except FileNotFoundError:
-            print(Fore.RED + '[!]  Not existing file or path' + Style.RESET_ALL)
+        main(args.input)
     else:
         print(Fore.RED + '[!]  No input file was given' + Style.RESET_ALL)
         sys.exit(1)
-
